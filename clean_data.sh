084325fd3044c3e13bb3c129cc4ae8bb639992fe
MaxiReglisse <ernest.chiarello@univ-fcomte.fr>
2015-07-07 12:19:12 +0200

    ajout de clean_data.sh

---
+++ clean_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# date dans les logs
+date >> /var/log/geosync/clean.log
+date >> /var/log/geosync/clean_error.log
+  
+# appel de clean.sh
+bash /home/georchestra-ouvert/bin/clean.sh 1>>/var/log/geosync/clean.log 2>>/var/log/geosync/clean_error.log
+
+# à inclure dans un crontab
+#tous les soirs de la semaine à 22h, nettoie le geoserver des couches qui ne sont plus partagées avec lui
+#0 22 * * 1-5   /home/georchestra-ouvert/bin/clean_data.sh
+
