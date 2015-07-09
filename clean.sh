f3ec14b24397217f2d1ef70a8238689d90de8b4e
MaxiReglisse <ernest.chiarello@univ-fcomte.fr>
2015-07-09 15:15:20 +0200

    new vector.sh

--- clean.sh
+++ clean.sh
@@ -24,7 +24,7 @@ echo $(xmllint --xpath "$xp" "$input" 2>/dev/null )
 main() {
   # chemin du script pour pouvoir appeler d'autres scripts dans le même dossier
   BASEDIR=$(dirname "$0")
-  echo "BASEDIR:$BASEDIR"
+  #echo "BASEDIR:$BASEDIR"
   
   local OPTIND opt
   while getopts "adsvh" opt; do
