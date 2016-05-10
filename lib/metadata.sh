0dd4a452166a6e01e57ceb6ae8666c21264c413c
MaxiReglisse <ernest.chiarello@univ-fcomte.fr>
2016-05-10 10:04:10 +0200

    pb de forme

--- lib/metadata.sh
+++ lib/metadata.sh
@@ -21,7 +21,7 @@ metadata::publish() {
   }
 
   local DIR
-  #chemin du script pour pouvoir appeler d'autres scripts dans le même dossier
+  # chemin du script pour pouvoir appeler d'autres scripts dans le même dossier
   DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
   #readonly DIR
 
@@ -79,7 +79,7 @@ metadata::publish() {
     return 1 # erreur
   fi
 
-  #valeurs des paramètres par défaut
+  # valeurs des paramètres par défaut
 
   # par défault output prend la valeur du nom du fichier source sans son extension
   # tic/tac/toe.shp.xml -> toe
@@ -90,15 +90,15 @@ metadata::publish() {
     output=$(util::cleanName "$input" -p)
   fi
 
-  #test si le fichier (xml) existe
-  #si le fichier n'existe pas, alors quitter
+  # teste si le fichier (xml) existe
+  # si le fichier n'existe pas, alors quitter
   if [ ! -f "$input" ] ; then 
     echoerror "le fichier n'existe pas : $input"
-    #continue quand même pour publier les données par défaut  #return 1 # erreur 
+    # continue quand même pour publier les données par défaut  #return 1 # erreur 
   fi
 
-  #retourne la valeur de l'expression xpath évalue sur le fichier $input
-  #ex: $(xpath "/metadata/dataIdInfo/idCitation/date/pubDate/text()") -> 2015-01-29T00:00:00
+  # retourne la valeur de l'expression xpath évalue sur le fichier $input
+  # ex: $(xpath "/metadata/dataIdInfo/idCitation/date/pubDate/text()") -> 2015-01-29T00:00:00
   xpath() { 
     local xp=$1 
     echo $(xmllint --xpath "$xp" "$input" 2>/dev/null )
@@ -107,7 +107,7 @@ metadata::publish() {
     # TODO: faire tout de même un test, une fois sur le fichier, de la validité du xml
   } 
 
-  #récupére (avec xpath) les métadonnées au format INSPIRE d'ArcGIS depuis le .shp.xml
+  # récupére (avec xpath) les métadonnées au format INSPIRE d'ArcGIS depuis le .shp.xml
   metadata=$(xpath "//text()")
   echo $metadata
   title=$(xpath "/metadata/dataIdInfo/idCitation/resTitle/text()") 
@@ -128,10 +128,10 @@ $pathx
 CMD
   }
 
-  #si le titre, le résumé, l'auteur ou la date de publication n'est pas trouvé avec leurs balises au format INSPIRE 
-  #ils sont recherchés avec leurs balises au format ISO 19139
-  #ils sont alors recherchés avec leurs balises ISO19139 de QSphere
-  #s'ils ne sont pas du tout renseignés dans le xml alors ils sont remplacés par une variante
+  # si le titre, le résumé, l'auteur ou la date de publication ne sont pas trouvés avec leurs balises au format INSPIRE 
+  # ils sont recherchés avec leurs balises au format ISO 19139
+  # ils sont alors recherchés avec leurs balises ISO19139 de QSphere
+  # s'ils ne sont pas du tout renseignés dans le xml alors ils sont remplacés par une variante
   if [ ! "$title" ]; then
     title=$(xpath "/metadata/idinfo/citation/citeinfo/title/text()")
     if [ ! "$title"]; then
@@ -202,8 +202,8 @@ CMD
 
   ## --------- Vecteurs issus de postgis  --------------
 
-  #attention : spécifier le shp concerné en fin d'url
-  #http://docs.geoserver.org/2.6.x/en/user/rest/api/datastores.html#workspaces-ws-datastores-ds-file-url-external-extension
+  # attention : spécifier le shp concerné en fin d'url
+  # http://docs.geoserver.org/2.6.x/en/user/rest/api/datastores.html#workspaces-ws-datastores-ds-file-url-external-extension
   local statuscode
   statuscode=$(curl --verbose --output /dev/null -w %{http_code} -u "$login:$password" -XPUT -H "Content-type: text/xml" \
     -d "<featureType><title>$title</title>
@@ -216,7 +216,7 @@ Chemin : $shppath</abstract>
   metadata: $metadata </description></featureType>" \
      "$url/geoserver/rest/workspaces/$workspace/datastores/postgis_data/featuretypes/$output"   2>&1)
   #--output /dev/null
-  #<enabled>true</enabled><advertised>true</advertised> est nécessaire pour éviter que la couche ne soit dépubliée (car par défaut "enabled" est mis à false lors d'un update
+  # <enabled>true</enabled><advertised>true</advertised> est nécessaire pour éviter que la couche ne soit dépubliée (car par défaut "enabled" est mis à false lors d'un update
   # pour rajouter des mots clés
   # ...</description><keywords><string>my_keyword1</string><string>my_keyword2</string></keywords></featureType>
 
@@ -273,5 +273,6 @@ if [ "${BASH_SOURCE[0]}" == "$0" ]; then
   main "$@"
 fi
    
-#source d'inpiration pour le style du code bash https://google-styleguide.googlecode.com/svn/trunk/shell.xml
-#outil pour vérifier la qualité du code : http://www.shellcheck.net/
+# source d'inpiration pour le style du code bash https://google-styleguide.googlecode.com/svn/trunk/shell.xml
+# outil pour vérifier la qualité du code : http://www.shellcheck.net/
+
