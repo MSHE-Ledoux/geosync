78724d53751c70165d910c2c1e9147f2b1d7b168
MSHE-Ledoux <thema-info@univ-fcomte.fr>
2016-05-17 15:31:52 +0200

    new git_seturl.sh

--- git_seturl.sh
+++ git_seturl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # positionner l'url sur github en ssh afin de pouvoir utiliser la clé ssh
-git remote set-url origin git@github.com:MaxiReglisse/geosync.git
+git remote set-url origin git@github.com:MSHE-Ledoux/geosync.git
 
