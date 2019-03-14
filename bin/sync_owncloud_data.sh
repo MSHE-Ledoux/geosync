83a0e8520d0fdbc2b57401155eba9b263e8d64ad
katoyuji <kato.yuji@gmail.com>
2019-03-14 09:14:25 +0100

    Update sync_owncloud_data.sh
    rajout de la liste d'exclusion

--- bin/sync_owncloud_data.sh
+++ bin/sync_owncloud_data.sh
@@ -33,7 +33,7 @@ echo_ifverbose() {
 # pour dépublier des couches, les déplacer dans le répertoire _unpublished
 #cmd="rsync --quiet -avr --delete --exclude 'lost+found' --exclude __*/ --exclude _unpublished '$INPUT_OUTPUT_PATH/' '$INPUT_COPY_PATH/'"
 # cmd="owncloudcmd --silent --unsyncedfolders $HOME/folder-to-exclude.lst --user $ocl_login --password $ocl_passwd $INPUT_COPY_PATH $ocl_host"
-cmd="owncloudcmd --silent --user $ocl_login --password $ocl_passwd $INPUT_COPY_PATH $ocl_host"
+cmd="owncloudcmd --silent --unsyncedfolders $HOME/folder-to-exclude.lst --user $ocl_login --password $ocl_passwd $INPUT_COPY_PATH $ocl_host"
 echo_ifverbose $cmd
 eval $cmd
 
